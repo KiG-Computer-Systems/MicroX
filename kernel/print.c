0c3ba0e49ed04c00347d1966a63e59366ac41fe2
Transistor <misteranthems5@gmail.com>
2026-03-04 23:02:13 +0500

    Improve print function to handle newlines
    Enhanced the print function to handle newline characters by adding carriage return and line feed support.

--- kernel/print.c
+++ kernel/print.c
@@ -1,13 +1,29 @@
-// print.c - simple text output using BIOS teletype
+// print.c - improved text output using BIOS teletype
 #include "kernel.h"
 
 void print(const char* str) {
     while (*str) {
-        __asm__ __volatile__ (
-            "movb $0x0E, %%ah\n\t"
-            "int $0x10"
-            : : "a"(*str)
-        );
+        if (*str == '\n') {
+            __asm__ __volatile__ (
+                "movb $0x0E, %%ah\n\t"
+                "movb $0x0D, %%al\n\t"   // CR
+                "int $0x10\n\t"
+                "movb $0x0E, %%ah\n\t"
+                "movb $0x0A, %%al\n\t"   // LF
+                "int $0x10\n\t"
+                :
+                :
+                : "ax"
+            );
+        } else {
+            __asm__ __volatile__ (
+                "movb $0x0E, %%ah\n\t"
+                "int $0x10\n\t"
+                :
+                : "a"(*str)
+                : "ax"
+            );
+        }
         str++;
     }
 }
